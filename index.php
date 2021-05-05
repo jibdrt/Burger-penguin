9237d322e137186c0d3500def4475b29cfd1d546
JbDruet <jb.druet@codeur.online>
2021-05-05 11:48:04 +0200

    responsiveNav

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="nav mx-auto">
+
+            
+            <img src="bgmenu.png" alt="" id="bg" onclick="test()">
+       
+        <img src="ping.png" id="ping" alt="" class="me-5">
+        <a href="" class="nav__item" id="ho">Home</a>
+
+        <span class="pipe">|</span>
+        
+        <a href="" class="nav__item" id="mo">More</a>
+        <span class="pipe">|</span>
+        <a href="" class="nav__item" id="fu">Find Us</a>
+        <span class="pipe">|</span>
+        <a href="" class="nav__item" id="he">Help</a>
+
+
+
+
+    </div>
+
+        <div id="burgerlist">
+            <div class="burgerlist__style"><a href="" class="cancelblue">Home</a></div>
+            <div class="burgerlist__style"><a href="" class="cancelblue">More</a></div>
+            <div class="burgerlist__style"><a href="" class="cancelblue">Find Us</a></div>
+            <div class="burgerlist__style"><a href="" class="cancelblue">Help</a></div>
+        </div>
+
+    
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function test() {
+    var x = document.getElementById('burgerlist');
+    if (x.style.display === 'none'){
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+
+    
+  };
+
+
+
+/*function test() {
+    document.getElementById('bg').click();
+    list.style.display = 'block';
+    
+  };
+*/
