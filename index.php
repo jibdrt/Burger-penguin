6fba91167b95ea028935f18f4dc6135ac82311db
JbDruet <jb.druet@codeur.online>
2021-10-12 20:44:53 +0200

    adding_Jquery-animations

--- index.php
+++ index.php
@@ -15,7 +15,56 @@
   <div class="div_scroll">
 
     <div class="topgap"></div>
-        <div class="spaceproject"></div>
+        <div class="spaceproject">
+        <div id="contentproject" class="wrap">
+                    <div class="inner">
+                        <div class="cards"></div>
+                    </div>  
+                </div>
+
+<style>
+
+    .cards{
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+    }
+
+    .wrap{
+        display: grid;
+        grid-template-rows: repeat(4, 1fr);
+        margin-left: 1vw;
+    }
+
+    .inner{
+        z-index: 0;
+        width: 100vw;
+        height: 800px;
+        border: none;
+        border-color: #222222;
+        background-color: #222222;
+    }
+
+    .cards__project{
+        margin-left: 200px !important;
+    }
+
+    .closebtn{
+        z-index: 999;
+        margin-top: 48px;
+        margin-left: 48px;
+        cursor: pointer !important;
+        border: 1px red solid;
+    }
+
+    .cardtrial{
+        height: 500px;
+        width: 300px;
+        border: 1px white solid;
+    }
+
+
+</style>
+        </div>
 
               <div class="content name">
 
@@ -89,7 +138,7 @@
 
 
                         $('.switch_project').click(function(){
-                            $('.spaceproject').load("projectpage.php").show();
+                            $('.spaceproject').load("projectpage.php").show("slide", { direction: "left" }, "slow");
                             $('#backbtn').show("slide", { direction: "right" }, 1000);
 
                         });
